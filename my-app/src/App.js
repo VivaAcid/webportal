ba69523eed79784b726c6e8fbafbbe356e8cf4ec
Bai-ee <bryanballi@gmail.com>
2021-05-15 15:17:20 -0500

    Design 1 built out

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,131 +1,38 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import dripLogo from './drip-logo.svg';
+// import stackLogo from './stack-logo.svg';
+
+
+
+import bgMobile from './lp-mobile.svg';
+// import bgMd from './lp-md.svg';
+import bgLg from './lp-lg.svg';
+
+
+console.log(bgLg)
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" class="w-screen h-screen overflow-hidden flex items-center justify-center">
+      
+      {/* <div id="logo" class="max-w-7xl"> */}
 
-<nav id="header" class="w-full z-30 top-0 text-white py-1 lg:py-6">
-      <div
-        class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-2 lg:py-6"
-      >
-        <div class="pl-4 flex items-center">
-          <a
-            class="text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
-            href="#"
-          >
-            <svg
-              class="h-6 w-6 inline-block fill-current text-yellow-700"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <path d="M13 8V0L8.11 5.87 3 12h4v8L17 8h-4z" />
-            </svg>
-            Bolt App
-          </a>
-        </div>
+      {/* </div> */}
+      {/* <div id="bg fixed z-20"> */}
 
-        <div class="block lg:hidden pr-4">
-          <button
-            id="nav-toggle"
-            class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-800 hover:border-green-500 appearance-none focus:outline-none"
-          >
-            <svg
-              class="fill-current h-3 w-3"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
-        </div>
+        <img src={dripLogo} class="z-20 max-h-full p-20 fixed"></img>
 
-        <div
-          class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 text-black p-4 lg:p-0 z-20"
-          id="nav-content"
-        >
-          <ul class="list-reset lg:flex justify-end flex-1 items-center">
-            <li class="mr-3">
-              <a
-                class="inline-block py-2 px-4 text-black font-bold no-underline"
-                href="#"
-                >Active</a
-              >
-            </li>
-            <li class="mr-3">
-              <a
-                class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
-                href="#"
-                >link</a
-              >
-            </li>
-            <li class="mr-3">
-              <a
-                class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
-                href="#"
-                >link</a
-              >
-            </li>
-          </ul>
-          <button
-            id="navAction"
-            class="mx-auto lg:mx-0 hover:underline text-gray-800 font-extrabold rounded mt-4 lg:mt-0 py-4 px-8 shadow opacity-75"
-          >
-            Action
-          </button>
-        </div>
-      </div>
-    </nav>
+      {/* </div> */}
 
-    <div class="container mx-auto h-screen">
-      <div class="text-center px-3 lg:px-0">
-        <h1
-          class="my-4 text-2xl md:text-3xl lg:text-5xl font-black leading-tight"
-        >
-          Main Hero Message to sell yourself!
-        </h1>
-        <p
-          class="leading-normal text-gray-800 text-base md:text-xl lg:text-2xl mb-8"
-        >
-          Sub-hero message, not too long and not too short. Make it just right!
-        </p>
-
-        <button
-          class="mx-auto lg:mx-0 hover:underline text-gray-800 font-extrabold rounded my-2 md:my-6 py-4 px-8 shadow-lg w-48"
-        >
-          Sign Up
-        </button>
-        <a
-          href="#"
-          class="inline-block mx-auto lg:mx-0 hover:underline bg-transparent text-gray-600 font-extrabold my-2 md:my-6 py-2 lg:py-4 px-8"
-          >View Additional Action</a
-        >
+      <div id="bg fixed z-10">
+        <img src={bgLg} class="hidden sm:block w-screen h-screen bg-contain"></img>
+        <img src={bgMobile} class="sm:hidden"></img>
       </div>
 
-      <div class="flex items-center w-full mx-auto content-end">
-        <div
-          class="browser-mockup flex flex-1 m-6 md:px-0 md:m-12 bg-white w-1/2 rounded shadow-xl"
-        ></div>
-      </div>
-    </div>
 
 
 
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          VIVA! Acid
-        </a>
-      </header> */}
     </div>
   );
 }
